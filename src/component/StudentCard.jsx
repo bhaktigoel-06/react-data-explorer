94eb04ed0305652a70740e4afaec6e47009ed3dc
bhakti <bhaktigoel01@gmail.com>
2026-07-30 23:29:44 +0530

    image added

--- src/component/StudentCard.jsx
+++ src/component/StudentCard.jsx
@@ -2,20 +2,25 @@
 export default function StudentCard({ student }) {
   return (
     <div className="student-card">
+ <img src={student.image} alt={student.name} className="student-img" />
 
       <h2 className="student-name">
          🆔 {student.id} {student.name}
       </h2>
 
-      <p>📧 {student.status}</p>
+      <p><b>STATUS:</b> {student.status}</p>
 
-      <p>🎓 {student.species}</p>
+      <p><b>SPECIES:</b> {student.species}</p>
 
-      <p>👨🏾‍🤝‍👨🏼 {student.gender}</p>
+      <p><b>GENDER:</b> {student.gender}</p>
 
-      <p>🌏 {student.origin.name}</p>
+      <p><b>ORIGIN:</b> {student.origin.name}</p>
 
-      <p></p>
+      <p><b>LOCATION</b>{student.location.name}</p>
+
+     
+
+      <p><b>CREATED:</b>{student.created}</p>
 
     </div>
   );
